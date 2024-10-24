1e23764c6dfef078e607af3fb949a4b02dba44ca
pablojwittich <pablojwittich1998@gmail.com>
2024-10-24 10:42:36 -0300

    new themes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,19 @@
 --- Con este plugin integramos un tema al nvim
 return {
+  --"olivercederborg/poimandres.nvim",
+  --"wuelnerdotexe/vim-enfocado",
+  --"navarasu/onedark.nvim",
+  "miikanissi/modus-themes.nvim",
   --"rebelot/kanagawa.nvim",
   --"TheNiteCoder/mountaineer.vim",
   --"bluz71/vim-moonfly-colors",
   --"killitar/obscure.nvim",
-  "yorumicolors/yorumi.nvim",
+  --"yorumicolors/yorumi.nvim",
 
   lazy = false, -- para que se carge inmeditamente sino lo hara asincronica
   priority = 1000, -- para que se cargue antes de todos los demas files
   config = function() -- mediante esta funcion decimos que se carge el tema
-    vim.cmd([[colorscheme yorumi]])
+    vim.cmd([[colorscheme modus]])
   end,
   opts = {
     --vim.g.moonflyTransparent == true,
